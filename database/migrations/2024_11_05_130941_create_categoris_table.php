812378c28022aaa66ac2656c56f4e9afd7a7a412
Sabardi <Sabardibahari@gmail.com>
2024-11-06 14:30:32 +0800

    fix

--- database/migrations/2024_11_05_130941_create_categoris_table.php
+++ database/migrations/2024_11_05_130941_create_categoris_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categoris', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->string('slug');
             $table->string('icon')->nullable();
             $table->timestamps();
